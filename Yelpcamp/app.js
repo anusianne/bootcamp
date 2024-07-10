c7eb907bfbc6043cd0c66499eedb7a260d475b58
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-07-10 17:07:47 +0200

    aded ejs-mate

--- Yelpcamp/app.js
+++ Yelpcamp/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground.js");
 const methodOverride = require("method-override");
 const app = express();
@@ -15,6 +16,7 @@ db.once("open", () => {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
