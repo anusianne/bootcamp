1cd62e545b1d9348376228daf92e3bfccf7cc9db
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-07-17 16:26:25 +0200

    added reviews schema

--- Yelpcamp/app.js
+++ Yelpcamp/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const catchAsync = require("./utilities/catchAsync.js");
 const ExpressError = require("./utilities/ExpressError.js");
 const Campground = require("./models/campground.js");
@@ -23,6 +24,25 @@ app.engine("ejs", ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -84,6 +104,7 @@ app.delete(
 
 app.post(
   "/campgrounds/:id/reviews",
+  validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
