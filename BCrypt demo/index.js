1723fdae47e5bb7c1314e7899171ac986bbff99e
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-07-25 15:28:42 +0200

    added login view

--- BCrypt demo/index.js
+++ BCrypt demo/index.js
@@ -1,13 +1,48 @@
 const express = require("express");
 const app = express();
-const User = require("./models/user");
+const User = require("./models/user.js");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-app.set("views", "views");
+mongoose.connect("mongodb://127.0.0.1:27017/loginDemo");
+
 app.set("view engine", "ejs");
+app.set("views", "views");
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("HOMEPAGE");
+});
 app.get("/register", (req, res) => {
   res.render("register");
 });
+
+app.post("/register", async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", async (req, res) => {
+  const { password, username } = req.body;
+  const user = await User.findOne({ username });
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (validPassword) {
+    res.send("YAAY, WELCOME");
+  } else {
+    res.send("invalid.");
+  }
+});
+
 app.get("/secret", (req, res) => {
   res.send("THIS IS SECRET.");
 });
