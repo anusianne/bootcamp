48aaa644e3b9314bef0389b084eed9e4f8750a39
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-07-25 14:10:01 +0200

    added new directory file and model

---
+++ BCrypt demo/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "cannot be blank."],
+  },
+  password: {
+    type: String,
+    required: [true, "cannot be blank."],
+  },
+});
+
+module.exports = mongoose.model("User, userSchema");
