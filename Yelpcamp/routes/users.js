181d439802f8e8c1212910113e53f821d5ab0750
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-07-30 14:22:12 +0200

    added basic form to register route file

--- Yelpcamp/routes/users.js
+++ Yelpcamp/routes/users.js
@@ -5,5 +5,7 @@ const User = require("../models/user");
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
-
+router.post("/register", async (req, res) => {
+  res.send(req.body);
+});
 module.exports = router;
