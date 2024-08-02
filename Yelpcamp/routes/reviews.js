c324a86bd3a3ff77be88f9b6bcff48443381be47
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-08-02 22:59:40 +0200

    added changes

--- Yelpcamp/routes/reviews.js
+++ Yelpcamp/routes/reviews.js
@@ -3,14 +3,16 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utilities/catchAsync.js");
 const Review = require("../models/review.js");
 const Campground = require("../models/campground.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
