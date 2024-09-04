2a796e8dd024fb7efc90290c453c2fcba320df0d
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-09-04 22:30:09 +0200

    added randomChoice function

--- First-React-Project/first-react-app/src/App.jsx
+++ First-React-Project/first-react-app/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Greeter from "./Greeter";
-import Die from "../Die";
-import ListPicker from "./ListPicker";
-import DoubleDice from "./DoubleDice";
-import ColorList from "./ColorList";
-import Game from "./Game";
-import ShoppingList from "./ShoppingList";
-import RentalProperties from "./RentalProperties";
-import Clicker from "./Clicker";
-import Form from "./Form";
-import Counter from "./Counter";
-import Toggler from "./Toggler";
-import TogglerCounter from "./TogglerCounter";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import Greeter from "./Greeter";
+// import Die from "../Die";
+// import ListPicker from "./ListPicker";
+// import DoubleDice from "./DoubleDice";
+// import ColorList from "./ColorList";
+// import Game from "./Game";
+// import ShoppingList from "./ShoppingList";
+// import RentalProperties from "./RentalProperties";
+// import Clicker from "./Clicker";
+// import Form from "./Form";
+// import Counter from "./Counter";
+// import Toggler from "./Toggler";
+// import TogglerCounter from "./TogglerCounter";
 import Box from "./Box";
 
 // const data = [
@@ -24,14 +24,14 @@ import Box from "./Box";
 //   { id: 4, item: "carrots", qty: 6, completed: true },
 // ];
 
-const properties = [
-  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-];
+// const properties = [
+//   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+//   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+//   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+//   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+//   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+//   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+// ];
 
 const colors = ["red", "blue", "black", "pink", "orange"];
 
--- First-React-Project/first-react-app/src/Box.jsx
+++ First-React-Project/first-react-app/src/Box.jsx
@@ -1,9 +1,20 @@
 import "./Box.css";
+import { useState } from "react";
 
-export default function Box({ colors }) {
+function randomChoice(arr) {
+  const idx = Math.floor(Math.random() * arr.length)
+  return arr[idx];
+}
+
+export default function Box({colors}) {
+  const [color, setColor] = useState("green");
+ const randomColor = randomChoice(colors)
+  const changeColor = () => {
+    setColor(randomColor)
+  }
   return (
     <div>
-      <div className="Box" style={{ backgroundColor: colors }}></div>
+      <div onClick={changeColor} className="Box" style={{ backgroundColor: color }}></div>
     </div>
   );
 }
