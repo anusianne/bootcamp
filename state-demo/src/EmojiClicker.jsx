6374a62ffb10e5bbc9272e25e7831838a9543ec8
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-10-12 18:32:13 +0200

    added randomEmoji and delete methos

--- state-demo/src/EmojiClicker.jsx
+++ state-demo/src/EmojiClicker.jsx
@@ -1,15 +1,29 @@
 import { v4 as uuid } from "uuid";
 import { useState } from "react";
 
+function randomEmoji() {
+  const emojiList = ["💀", "🟠", "🩷", "😍", "😈"];
+  return emojiList[Math.floor(Math.random() * emojiList.length)];
+}
+
 export default function EmojiClicker() {
-  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: ":)" }]);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmoji() }]);
   const addEmoji = () => {
-    setEmojis([...emojis, { id: uuid(), emoji: ":D" }]);
+    setEmojis([...emojis, { id: uuid(), emoji: randomEmoji() }]);
+  };
+  const deleteEmoji = (id) => {
+    setEmojis((prevEmojis) => {
+      return prevEmojis.filter((e) => e.id !== id);
+    });
   };
   return (
     <div>
       {emojis.map((e) => (
-        <span key={e.id} style={{ fontSize: "4em" }}>
+        <span
+          onClick={() => deleteEmoji(e.id)}
+          key={e.id}
+          style={{ fontSize: "4em" }}
+        >
           {e.emoji}
         </span>
       ))}
