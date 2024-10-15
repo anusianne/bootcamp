0dedea3a1f69b6b9e303a63f4a7733d91b7ef29b
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-10-15 22:30:38 +0200

    added score keeper

--- state-demo/src/App.jsx
+++ state-demo/src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Counter from "./Counter";
 import ScoreKeeper from "./ScoreKeeper";
-import EmojiClicker from "./EmojiClicker";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>State demo</h1>
-      <EmojiClicker />
+      
+      <ScoreKeeper numPlayers={10} target={3}/>
     </div>
   );
 }
