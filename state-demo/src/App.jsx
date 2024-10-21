ca81bee2abd4f346634bebb253108d3c6ae42f54
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-10-21 20:06:34 +0200

    added function as a prop

--- state-demo/src/App.jsx
+++ state-demo/src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
 import LuckyN from "./LuckyN";
+import { sum } from "./utils";
+
+function lessThan4(dice) {
+  return sum(dice) < 4;
+}
+
+function sameNumber(dice) {
+  return dice.every((v) => v === dice[0]);
+}
+
 function App() {
   return (
     <div>
-      <LuckyN />
+      <LuckyN winCheck={lessThan4} />
+      <LuckyN winCheck={sameNumber} />
     </div>
   );
 }
