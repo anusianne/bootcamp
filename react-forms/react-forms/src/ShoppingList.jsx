41605387e7e3068ae3b2c805696e08d0210c7b25
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-10-28 19:08:45 +0100

    added unique id for formData

--- react-forms/react-forms/src/ShoppingList.jsx
+++ react-forms/react-forms/src/ShoppingList.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import ShoppingListForm from "./ShoppingListForm";
+import { v4 as uuid } from "uuid";
 
 export default function ShoppingList() {
   const [items, setItems] = useState([
-    { id: 1, product: "Bananas", quantity: 8 },
-    { id: 2, product: "Eggss", quantity: 20 },
+    { id: uuid(), product: "Bananas", quantity: 8 },
+    { id: uuid(), product: "Eggss", quantity: 20 },
   ]);
   const addItem = (item) => {
     setItems((currItems) => {
-      return [...currItems, { ...item, id: 9 }];
+      return [...currItems, { ...item, id: uuid() }];
     });
   };
   return (
