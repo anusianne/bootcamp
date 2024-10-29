9149d171ec36068b3b740d2b72e72bbc15b892ca
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-10-29 22:12:53 +0100

    added validate function

--- react-forms/react-forms/src/ValidatedShoppingListForm.jsx
+++ react-forms/react-forms/src/ValidatedShoppingListForm.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
 
-export default function ValidatedShoppingListForm() {
-  const [formData, setFormData] = useState({ product: "", qty: 0 });
+export default function ValidatedShoppingListForm({addItem}) {
+  const [formData, setFormData] = useState({ product: "", quantity: 0 });
   const [productIsValid, setProductIsValid] = useState(false)
+  const validate = (product) => {
+    if (product.length === 0) {
+        setProductIsValid(false)
+    } else {
+        setProductIsValid(true)
+    }
+  }
+  
   const handleChange = (e) => {
+    if (e.target.name === 'product') {
+        validate(e.target.value)
+    }
     setFormData((currData) => {
       return { ...currData, [e.target.name]: e.target.value };
     });
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(productIsValid) {
+        addItem(formData);
+        setFormData({product: '', quantity: 0})
+    }
+  }
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
         <h1>Product is: {formData.product}</h1>
       <label htmlFor="product">Product name: </label>
       <input
@@ -22,15 +40,16 @@ export default function ValidatedShoppingListForm() {
       />
       {!productIsValid && ( <p style={{color: "red"}}>Product name canoot be empty!</p>)}
       <h1>Qty is: {formData.qty}</h1>
-      <label htmlFor="qty">Product name: </label>
+      <label htmlFor="quantity">Product name: </label>
       <input
         type="number"
         placeholder="1"
-        name="qty"
-        id="qty"
+        name="quantity"
+        id="quantity"
         onChange={handleChange}
         value = {formData.qty}
       />
+      <button onClick={handleSubmit}>Add Item</button>
     </form>
   );
 }
