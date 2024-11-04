ac643c3b4471c5ed27cfd1bc6383157e737955b4
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-11-04 17:01:27 +0100

    added todolist new project

---
+++ react-todolist/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <CssBaseline />
+    </>
+  );
+}
+
+export default App;
