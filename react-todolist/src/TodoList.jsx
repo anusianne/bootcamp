437b053baa74ae76ac65c87e30adef544b187f9e
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-11-05 12:13:50 +0100

    added toggling todos

--- react-todolist/src/TodoList.jsx
+++ react-todolist/src/TodoList.jsx
@@ -16,6 +16,17 @@ export default function TodoList() {
       return prevTodos.filter((t) => t.id !== id);
     });
   };
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo) => (
@@ -23,6 +34,7 @@ export default function TodoList() {
           todo={todo}
           key={todo.id}
           removeTodo={() => removeTodo(todo.id)}
+          toggle={() => toggleTodo(todo.id)}
         />
       ))}
     </List>
